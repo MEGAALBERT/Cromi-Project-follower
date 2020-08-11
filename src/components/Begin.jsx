fd8bf95c2ade791cd18798222dd84bb5913e6dd1
Alberto Medellin <albertomedellin85@gmail.com>
2020-08-11 02:59:29 -0700

    On creating user login for club members

--- src/components/Begin.jsx
+++ src/components/Begin.jsx
@@ -14,7 +14,13 @@ export default function Begin() {
     return (
         <div className="main" style={myStyle}>
             <h1 className="storyTitle">Bienvenidos a CLUB ROTARIO MEXICALI INDUSTRIAL</h1>
-            <img src={logo} alt="Mexicali" />
+            <img src={logo} alt="Mexicali" /><br/>
+            <h2 className="storyTitle">Login Solo para Miembors del Club</h2>
+            <label htmlFor="name"> Correo Electronico </label>
+            <input type="text" className="form-control" />
+            <label htmlFor="name"> Password </label>
+            <input type="text" className="form-control" />
+            <button type="submit" className="btn btn-primary"> Entrar </button>
         </div>
     )
 }
