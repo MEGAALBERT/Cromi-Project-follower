13dcdf8ec1fb725982b9f4366d789455883e0966
Alberto Medellin <albertomedellin85@gmail.com>
2020-08-11 17:58:20 -0700

    Fixing server so it deploys to heroku

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const path = require("path");
 const db = require("./knex.js");
 const app = express();
-// Setup logger
+// Setup logger !!
 app.use(
   morgan(
     ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
